0b8c115a5797bcb9d46135299191bb981cb1f487
Patryk Mrukot <ptrk.mrukot@gmail.com>
2024-05-20 17:01:23 +0200

    More complex state

--- src/components/TodoListLocalState.tsx
+++ src/components/TodoListLocalState.tsx
@@ -1,117 +1,124 @@
 import React, { useState } from "react";
 
-export const TodoListLocalState = () => {
-    const [todoList, setTodoList] = useState<string[]>([]);
-    const [taskList, setTaskList] = useState<any[]>([]);
-    const [task, setTask] = useState({
-        title: "",
-        description: "",
-        priority: "",
-        dueDate: ""
-    });
+type Priority = "low" | "mid" | "high";
 
-    const addTodoItem = () => {
-        setTodoList(todoList.concat(["todo"]));
-    };
-
-    const handleChange = (e:any) => {
-        const { name, value } = e.target;
-        setTask((prevTask) => ({
-            ...prevTask,
-            [name]: value
-        }));
-    };
-
-    const handleSubmit = (e:any) => {
-        setTaskList((prevTaskList) => [...prevTaskList, task]);
-        setTask({ title: "", description: "", priority: "", dueDate: "" });
-        e.preventDefault();
-    };
-
-    return (
-        <div>
-            <h3>Todo list local state</h3>
-            <button data-test-button="add-todo" onClick={addTodoItem}>
-                Click to add todo
-            </button>
-            <ul>
-                {todoList
-                    .map((todoItem, index) => `${todoItem}-${index + 1}`)
-                    .map((item) => (
-                        <li data-test-li="todo-item" key={item}>
-                            {item}
-                        </li>
-                    ))}
-            </ul>
-
-            <div className="task-list">
-                <h2>Lista zadań</h2>
-                <ul>
-                    {taskList.map((task, index) => (
-                        <li key={index}>
-                            <div className="task-title">Title: {task.title}</div>
-                            <div className="task-details">Description: {task.description}</div>
-                            <div className="task-details">Priority: {task.priority}</div>
-                            <div className="task-details">Due Date: {task.dueDate}</div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-
-            <form id="toDo" onSubmit={handleSubmit}>
-                <h3>Add Task</h3>
-                <div>
-                    <label>Title </label>
-                    <input
-                        id="taskTitle"
-                        type="text"
-                        name="title"
-                        value={task.title}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Description </label>
-                    <textarea
-                        id="taskDescription"
-                        name="description"
-                        value={task.description}
-                        onChange={handleChange}
-                        required
-                    ></textarea>
-                </div>
-                <div>
-                    <label htmlFor="taskPriority">Priority </label>
-                    <select
-                        id="taskPriority"
-                        name="priority"
-                        value={task.priority}
-                        onChange={handleChange}
-                        required
-                    >
-                        <option value="low">low</option>
-                        <option value="mid">mid</option>
-                        <option value="high">high</option>
-                    </select>
-                </div>
-                <div>
-                    <label>Due Date </label>
-                    <input
-                        type="date"
-                        id="taskDueDate"
-                        name="dueDate"
-                        value={task.dueDate}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <button type="submit">Add Task</button>
-            </form>
-        </div>
-    );
+type Task = {
+  title: string;
+  description: string;
+  priority: Priority;
+  dueDate: string;
 };
 
+type TodoListProps = {
+  todoList: Task[];
+};
+
+const TodoList: React.FC<TodoListProps> = ({ todoList }) => (
+  <div className="task-list">
+    <h2>Lista zadań</h2>
+    <ul>
+      {todoList.map((task, index) => (
+        <li key={index}>
+          <div className="task-title">Title: {task.title}</div>
+          <div className="task-details">Description: {task.description}</div>
+          <div className="task-details">Priority: {task.priority}</div>
+          <div className="task-details">Due Date: {task.dueDate}</div>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+type TodoListFormProps = {
+  setTaskList: React.Dispatch<React.SetStateAction<Task[]>>;
+};
+
+const TodoListForm: React.FC<TodoListFormProps> = ({ setTaskList }) => {
+  const [task, setTask] = useState<Task>({
+    title: "",
+    description: "",
+    priority: "low",
+    dueDate: "",
+  });
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setTask((prevTask) => ({
+      ...prevTask,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    setTaskList((prevTaskList) => [...prevTaskList, task]);
+    setTask({ title: "", description: "", priority: "low", dueDate: "" });
+    e.preventDefault();
+  };
+
+  return (
+    <form id="toDo" onSubmit={handleSubmit}>
+      <h3>Add Task</h3>
+      <div>
+        <label>Title </label>
+        <input
+          id="taskTitle"
+          type="text"
+          name="title"
+          value={task.title}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Description </label>
+        <textarea
+          id="taskDescription"
+          name="description"
+          value={task.description}
+          onChange={handleChange}
+          required
+        ></textarea>
+      </div>
+      <div>
+        <label htmlFor="taskPriority">Priority </label>
+        <select
+          id="taskPriority"
+          name="priority"
+          value={task.priority}
+          onChange={handleChange}
+          required
+        >
+          <option value="low">low</option>
+          <option value="mid">mid</option>
+          <option value="high">high</option>
+        </select>
+      </div>
+      <div>
+        <label>Due Date </label>
+        <input
+          type="date"
+          id="taskDueDate"
+          name="dueDate"
+          value={task.dueDate}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">Add Task</button>
+    </form>
+  );
+};
+
+export const TodoListLocalState = () => {
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  return (
+    <div>
+      <TodoList todoList={taskList} />
+      <TodoListForm setTaskList={setTaskList} />
+    </div>
+  );
+};
 
 // import React, { useState } from "react";
 //
